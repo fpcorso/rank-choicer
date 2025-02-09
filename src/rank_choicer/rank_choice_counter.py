ce7ae222e1f456ecd106cfb2a66d6e0fa4b7ebce
Frank Corso <frank@frankcorso.me>
2025-02-09 11:30:48 -0500

    Implement methods to add or remove specific options

--- src/rank_choicer/rank_choice_counter.py
+++ src/rank_choicer/rank_choice_counter.py
@@ -39,3 +39,33 @@ class RankChoiceCounter:
             raise ValueError("Duplicate options are not allowed")
 
         self._options = new_options.copy()
+
+    def add_option(self, option: str) -> None:
+        """
+        Add a new valid option to the voting options.
+
+        Args:
+            option: New option to add
+
+        Raises:
+            ValueError: If option already exists
+        """
+        if option in self._options:
+            raise ValueError("Option already exists")
+
+        self._options.append(option)
+
+    def remove_option(self, option: str) -> None:
+        """
+        Remove an option from the valid voting options.
+
+        Args:
+            option: Option to remove
+
+        Raises:
+            ValueError: If option doesn't exist
+        """
+        if option not in self._options:
+            raise ValueError("Option does not exist")
+
+        self._options.remove(option)
